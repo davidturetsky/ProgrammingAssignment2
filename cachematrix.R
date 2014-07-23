3b0336d1dca13746424ee213a84635f6103774a0
David B Turetsky <david.b.turetsky@adp.com>
2014-07-22 21:33:23 -0400

    First Attempt
    I hope its a good one…

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It is my intent to submit the real function requested
+## that creats a matrix for the inversion process
+## then solving the inverse of the matrix from the cache
 
-## Write a short comment describing this function
+## Create the matrix to cache
 
 makeCacheMatrix <- function(x = matrix()) {
+        cm <- function(y) {
+                x <<- y
+        }
+        get <- function() x
 
 }
 
 
-## Write a short comment describing this function
+## A function that returns the inverse of the matrix x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- cm(x)
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(x)
+
+        return(m)
 }
